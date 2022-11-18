e7b1f9a08d82e7ac77539bbc6923c4b646ccbfc5
Egor <wupsssen@yandex.ru>
2022-11-18 21:52:00 +0600

    маша

---
+++ lesson1hello/newuser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя пользователя: ");
+string username = Console.ReadLine ();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("ура это же Маша!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
